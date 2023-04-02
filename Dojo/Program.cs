ca09e2b0c5a0dd929955a4f8b8563b42c982fe00
Rallenson <Rallenson900@gmail.com>
2023-04-02 00:15:44 -0300

    feat(Desafio 9): Desafio 9 Concluido

--- Dojo/Program.cs
+++ Dojo/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Globalization;
+using System.Threading;
 
 namespace Dojo
 {
@@ -16,12 +17,14 @@ namespace Dojo
             do
             {
                 Console.WriteLine("---------------------------\n\nMENU DO DOJO\n\n---------------------------\n");
-                Console.WriteLine("\nEscolha um número de 0 a 9 ou escreva 'sair':\n");
+                Console.WriteLine("\nEscolha um número de 0 a 15 ou escreva 'sair':\n");
                 Console.WriteLine("0)Sair:\n");
                 Console.WriteLine("1)Soma:\n");
                 Console.WriteLine("2)Conversão de metros para milímetros:\n");
                 Console.WriteLine("3)Calcular Aumento:\n");
                 Console.WriteLine("4)Calcular Desconto:\n");
+                Console.WriteLine("6)Calcular IMC:\n");
+                Console.WriteLine("9)Múltiplos de 3 entre 0 e 100:\n");
                 Console.Write("Resposta: ");
                 opcao = Console.ReadLine().ToLower();
 
@@ -156,27 +159,26 @@ namespace Dojo
                             Console.Write("\nDigite Sua Altura: ");
                             float Altura;
                             bool AlturaValid = float.TryParse(Console.ReadLine(), out Altura);
-                            float AlturaCenti = Altura / 100;
                             if (AlturaValid == false)
                             {
                                 Console.WriteLine("Essa Altura não é Válida.");
                             }
                             else
                             {
-                                double IMC = Peso / Math.Pow(AlturaCenti, 2);
-                                if(IMC < 18.5)
+                                double IMC = Peso / Math.Pow(Altura, 2);
+                                if (IMC < 18.5)
                                 {
                                     Consulta = "Abaixo Do Peso";
                                 }
-                                else if (IMC == 18.5 || IMC > 24.9)
+                                else if (IMC >= 18.5 && IMC <= 29.9)
                                 {
                                     Consulta = "No Peso Ideal";
                                 }
-                                else if (IMC == 30 || IMC > 34.9)
+                                else if (IMC >= 30 && IMC <= 34.9)
                                 {
                                     Consulta = "Na Obesidade grau 1";
                                 }
-                                else if (IMC == 35 || IMC > 39.9)
+                                else if (IMC >= 35 && IMC <= 39.9)
                                 {
                                     Consulta = "Na Obesidade grau 2";
                                 }
@@ -184,7 +186,6 @@ namespace Dojo
                                 {
                                     Consulta = "Obesidade grau 3";
                                 }
-                                IMC = IMC / 10000;
                                 Console.Write($"\n*****************************\n\nRESULTADO!!!!!\n\nVocê Está {Consulta}\n\nO IMC Do Peso {Peso} Na Altura {Altura:F2} é {IMC:F2}\n\n*****************************\n");
                             }
                         }
@@ -199,8 +200,50 @@ namespace Dojo
                         break;
                     case "9":
                         numero = 9;
-                        Console.WriteLine("\nVocê escolheu o número 9\n-----------------------------\n");
+                        Console.WriteLine("\n\n----------------------------\n\nVocê escolheu o número 9 - MÚLTIPLOS DE 3 ENTRE 0 E 100!!!\n\n----------------------------\n");
+                        Console.Write("Resultado!!!\n\n*****************************\n\nOs Multiplos São: ");
+                        var Count = 0;
+                        while(Count < 100)
+                        {
+                            Count = Count + 1;
+                            if (Count % 3 == 0)
+                            {
+                                if(Count == 99)
+                                {
+                                    Console.WriteLine($"{Count} \n\n*****************************\n");
+                                }
+                                else
+                                {
+                                    Console.Write($"{Count},");
+                                }
+                            }
+                        }
                         break;
+                    case "10":
+                        numero = 9;
+                        Console.WriteLine("\nVocê escolheu o número 10\n-----------------------------\n");
+                        break;
+                    case "11":
+                        numero = 9;
+                        Console.WriteLine("\nVocê escolheu o número 11\n-----------------------------\n");
+                        break;
+                    case "12":
+                        numero = 9;
+                        Console.WriteLine("\nVocê escolheu o número 12\n-----------------------------\n");
+                        break;
+                    case "13":
+                        numero = 9;
+                        Console.WriteLine("\nVocê escolheu o número 13\n-----------------------------\n");
+                        break;
+                    case "14":
+                        numero = 9;
+                        Console.WriteLine("\nVocê escolheu o número 14\n-----------------------------\n");
+                        break;
+                    case "15":
+                        numero = 9;
+                        Console.WriteLine("\nVocê escolheu o número 15\n-----------------------------\n");
+                        break;
+
                     case "sair":
                         Console.WriteLine("\nVocê escolheu sair. Até a próxima!\n--------------------------------\n  ");
                         break;
